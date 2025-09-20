e9bb17bd907b811986b21a56a6dde0fa94ff1d3d
Anakha Shaji <anakhashaji@al.org>
2025-09-20 10:56:02 +0530

    modified

--- main.py
+++ main.py
@@ -9,13 +9,35 @@ def greet():
     return "welcome"
 
 products=[
-    Product(1,"phone","abcc",12,12000),Product(2,"laptop",
-            "abcc",12,12000)
+    Product(id=1,name="phone",description="abcc",price=12,quantity=12000),
+    Product(id=2,name="phone",description="abcc",price=12,quantity=12000),
+    Product(id=3,name="phone",description="abcc",price=12,quantity=12000),
+  
+  
+  
 ]
 
 @app.get("/products")
 def get_all_products():
     return products
+
+
+@app.get("/product/{id}")
+def get_product_by_id(id:int):
+    for product in products:
+        if product.id == id:
+            return product
+
+
+    return "product not found"
+
+@app.post("/product")
+def add_product(Product:Product):
+    products.append(Product)
+    return products
+
+
+
 
 
 # @app.get("/favicon.ico", include_in_schema=False)
